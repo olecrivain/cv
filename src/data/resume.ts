3499ceb0ada8cca745f0d58876438eb8e02f6f25
Olivier Lecrivain <olivier.lecrivain@gmail.com>
2023-12-27 14:53:31 +0100

    wip

---
+++ src/data/resume.ts
@@ -0,0 +1,11 @@
+// https://github.com/BartoszJarocki/cv/blob/main/src/data/resume-data.tsx
+
+export type Resume = {
+  name: string;
+  location: string;
+};
+
+export const resume: Resume = {
+  name: "Olivier Lecrivain",
+  location: "Paris, France",
+};
